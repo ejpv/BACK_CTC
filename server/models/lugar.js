ce00e3b8383d8515dfaaaf93cc84c8f1f1edb597
ejpv <erickjpv@hotmail.es>
2020-11-26 10:57:06 -0500

    Edición de endpoints de Lugar, aún en desarrollo

--- server/models/lugar.js
+++ server/models/lugar.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose')
 let Schema = mongoose.Schema
 
 let lugarSchema = new Schema({
-    provincia: { type: String },
-    canton: { type: String },
+    provincia: { type: String, required: [true, 'La provincia es necesaria'] },
+    canton: { type: String, required: [true, 'El cantón es necesario'] },
     ciudad: { type: String },
-    parroquia: { type: String },
-    lat: { type: String },
-    lon: { type: String },
+    parroquia: { type: String, required: [true, 'La parroquia es necesaria'] },
+    lat: { type: String, required: [true, 'La Latitud es necesaria'] },
+    lon: { type: String, required: [true, 'La Longitud es necesaria'] },
     estado: { type: Boolean, default: true },
 })
 
--- server/routes/lugar.js
+++ server/routes/lugar.js
@@ -1,22 +1,151 @@
 const express = require('express')
-const FormularioRepresentante = require('../models/formularioRepresentante')
+const Lugar = require('../models/lugar')
 const { verificarToken, verificarNotRepresentant } = require('../middlewares/autentication')
 const _ = require('underscore')
 const app = express()
 
 //generar un Lugar
-app.post('/api/lugar', [verificarToken, verificarNotRepresentant], (req, res)=>{})
+app.post('/api/lugar', [verificarToken, verificarNotRepresentant], (req, res) => {
+    let body = req.body
+
+    let lugar = new Lugar({
+        provincia: body.provincia,
+        canton: body.canton,
+        ciudad: body.ciudad,
+        parroquia: body.parroquia,
+        lat: body.lat,
+        lon: body.lon
+    })
+
+    lugar.save((err, lugarDB) => {
+        if (err) {
+            res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        res.json({
+            ok: true,
+            message: 'Lugar creado satisfactoriamente',
+            lugar: lugarDB
+        })
+    })
+})
 
 //Ver todos los lugares
-app.get('/api/lugares', [verificarToken, verificarNotRepresentant], (req, res)=>{})
+app.get('/api/lugares', [verificarToken, verificarNotRepresentant], (req, res) => {
+    // el estado por defecto es true, solo acepta estado falso por la url
+    const estado = req.query.estado === 'false' ? false : true
+
+    Lugar.find({ estado }).exec((err, lugares) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        Lugar.countDocuments({ estado }, (err, conteo) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                })
+            }
+
+            res.json({
+                ok: true,
+                total: conteo,
+                lugares
+            })
+        })
+    })
+})
 
 //Editar un lugar por id
-app.put('/api/lugar/:id', [verificarToken, verificarNotRepresentant], (req, res)=>{})
+app.put('/api/lugar/:id', [verificarToken, verificarNotRepresentant], (req, res) => {
+    let id = req.params.id
+    let body = _.pick(req.body, ['canton', 'provincia', 'parroquia', 'lat', 'lon', 'ciudad'])
+
+    Lugar.findByIdAndUpdate(id, body, { runValidators: true, context: 'query' },
+        (err, lugarDB) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                })
+            }
+
+            if (!lugarDB) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'No existe ese lugar, id inválido'
+                    }
+                })
+            }
+
+            if (lugarDB.estado === false) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'El lugar está actualmente borrado.'
+                    }
+                })
+            }
+
+          res.status(204).json({
+                ok: true,
+                message: 'Lugar borrado correctamente.'
+            })
+            //res.status(204).json()
+        })
+})
 
 //Eliminar un lugar por id
-app.put('/api/lugar/:id', [verificarToken, verificarNotRepresentant], (req, res)=>{})
+app.put('/api/lugar/:id', [verificarToken, verificarNotRepresentant], (req, res) => { })
 
 //Restaurar un lugar por id
-app.put('/api/lugar/:id/restaurar', [verificarToken, verificarNotRepresentant], (req, res)=>{})
+app.put('/api/lugar/:id/restaurar', [verificarToken, verificarNotRepresentant], (req, res) => {
+    let id = req.params.id
+    let cambiarEstado = {
+        estado: true
+    }
+
+    Lugar.findByIdAndUpdate(id, cambiarEstado, { runValidators: true, context: 'query' },
+        (err, lugarDB) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                })
+            }
+
+            if (!lugarDB) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'No existe ese lugar, id inválido'
+                    }
+                })
+            }
+
+            if (lugarDB.estado === false) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'El lugar está actualmente borrado.'
+                    }
+                })
+            }
+
+            /*res.json({
+                ok: true,
+                message: 'Lugar borrado correctamente.'
+            })*/
+            res.status(204)
+        })
+})
 
 module.exports = app
