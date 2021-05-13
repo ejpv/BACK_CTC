694c0c918d5079d95ac8ecc7a616607b8ee31119
ejpv <erickjpv@hotmail.es>
2021-05-12 19:02:13 -0500

    comentarios en establecimiento para proximas y posibles ediciones

--- server/models/establecimiento.js
+++ server/models/establecimiento.js
@@ -7,13 +7,27 @@ let establecimientoSchema = new Schema({
   nombre: { type: String, required: [true, 'El Nombre del establecimiento es obligatorio'] },
   representante: { type: Schema.Types.ObjectId, ref: 'Representante', },
   administrador: { type: String, required: [true, 'El Administrador es obligatorio'] },
+  //**************ELIMINAR LUGAR********
   lugar: { type: Schema.Types.ObjectId, ref: 'Lugar', required: [true, 'El Lugar es necesario'] },
+  //************************************
   registro: { type: String, required: false },
   LUAF: { type: String, required: false },
   email: { type: String, required: false, unique: false },
   nacionalidad: { type: String, required: false },
   web: { type: String, required: false },
   telefono: { type: String, required: false },
+  // provincia: { type: String },
+  // canton: { type: String },
+  // ciudad: { type: String },
+  // parroquia: { type: String },
+  // lat: { type: String },
+  // lng: { type: String },
+  // serviciosBasicos: {
+  //   agua: { type: String },
+  //   saneamiento: { type: String },
+  //   energia: { type: String },
+  //   desechos: { type: String }
+  // },
   areaProtegida: { type: Schema.Types.ObjectId, ref: 'AreaProtegida', required: false },
   estado: { type: Boolean, default: true }
 })
