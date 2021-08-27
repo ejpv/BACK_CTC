23961764762e74590b956d256ada26ebeb4775fd
ejpv <erickjpv@hotmail.es>
2021-08-27 13:34:59 -0500

    represenante agregado al obtener todos los informes aprobados

--- server/routes/informe.js
+++ server/routes/informe.js
@@ -150,12 +150,22 @@ app.get('/api/informes', verificarToken, async (req, res) => {
                     path: 'actividad', model: 'actividad'
                 }
             }
-        }).populate({
+        })
+        .populate({
             path: 'diagnostico', model: 'diagnostico',
             populate: {
                 path: 'establecimiento', model: 'establecimiento',
                 populate: {
                     path: 'areaProtegida', model: 'areaProtegida'
+                }
+            }
+        })
+        .populate({
+            path: 'diagnostico', model: 'diagnostico',
+            populate: {
+                path: 'establecimiento', model: 'establecimiento',
+                populate: {
+                    path: 'representante', model: 'representante'
                 }
             }
         })
